d56e20895c1cc7552ea58aaa4fd844ca047f475d
Clinton Cao <C.S.Cao@student.tudelft.nl>
2018-06-12 13:13:37 +0200

    Fixed Cas's mistakes and added init.py

---
+++ datareader.py
@@ -0,0 +1,21 @@
+class dataReader:
+
+	def __init__(self, filePath):
+		self.data = []
+
+		with open(filePath, 'r') as flowData:
+			count = 0
+			for line in flowData:
+				if count == 0:
+					count += 1
+					continue
+
+				splittedLine =  line.split("\t")
+
+				if len(splittedLine[4]) == 0:
+					self.data.append([splittedLine[0], splittedLine[1], splittedLine[2], splittedLine[3],splittedLine[6],splittedLine[7],splittedLine[8],splittedLine[9],splittedLine[10],splittedLine[11], splittedLine[12][:-1]])
+				else:
+					self.data.append([splittedLine[0], splittedLine[1], splittedLine[2], splittedLine[3],splittedLine[5],splittedLine[6],splittedLine[7],splittedLine[8],splittedLine[9],splittedLine[10], splittedLine[11][:-1]])
+
+	def get_data(self):
+		return self.data
