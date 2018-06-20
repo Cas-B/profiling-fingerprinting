eaf9ee7f97d643057c659a9404cbe8d1fe998e98
Cas-B <s.j.m.buijs@student.tudelft.nl>
2018-06-20 20:04:35 +0200

    Combines datahandling, adds discretization and visualisation

--- visualisation.py
+++ visualisation.py
@@ -1,57 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from filteredDataReader import dataReaderFiltered
+from datahandler import dataHandler
 
-reader = dataReaderFiltered("capture.labeled")
-netflows = np.array(reader.get_data())[:1000]
-infected_data = reader.get_data_from("147.32.84.165")[:1000]
+handler = dataHandler()
+handler.read_data("capture51.labeled")
+handler.filter_background_data()
+netflows = np.array(handler.get_filtered_data())
+infected = np.array(handler.get_ip_data("147.32.84.165"))
+protocols = [netflows[i][2] for i in range(len(netflows))]
+protocol = [infected[i][2] for i in range(len(infected))]
 
-column_infected = []
-for i in range(np.shape(infected_data)[0]):
-	column_infected.append(infected_data[i][6])
-#6,7,8,9
-column= []
-for i in range(np.shape(netflows)[0]):
-	column.append(netflows[i][6])
-
-plt.plot(column_infected, color="blue")
-plt.plot(column, color="red")
+print("Show relation between protocol use")
+plt.plot(protocols, color="red")
+plt.plot(protocol,color="blue")
 plt.show()
-
-column_infected = []
-for i in range(np.shape(infected_data)[0]):
-	column_infected.append(infected_data[i][7])
-#6,7,8,9
-column= []
-for i in range(np.shape(netflows)[0]):
-	column.append(netflows[i][7])
-
-plt.plot(column_infected, color="blue")
-plt.plot(column, color="red")
-plt.show()
-
-column_infected = []
-for i in range(np.shape(infected_data)[0]):
-	column_infected.append(infected_data[i][8])
-#6,7,8,9
-column= []
-for i in range(np.shape(netflows)[0]):
-	column.append(netflows[i][8])
-
-plt.plot(column_infected, color="blue")
-plt.plot(column, color="red")
-plt.show()
-
-column_infected = []
-for i in range(np.shape(infected_data)[0]):
-	column_infected.append(infected_data[i][9])
-#6,7,8,9
-column= []
-for i in range(np.shape(netflows)[0]):
-	column.append(netflows[i][9])
-
-plt.plot(column_infected, color="blue")
-plt.plot(column, color="red")
-plt.show()
-
