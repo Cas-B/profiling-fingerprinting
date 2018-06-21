40c4d0e2bbd1e0c1a175d614beb3b83d52b0a877
Clinton Cao <C.S.Cao@student.tudelft.nl>
2018-06-21 16:30:27 +0200

    Cast type of bytes to integer.

--- visualisation.py
+++ visualisation.py
@@ -12,20 +12,13 @@ infected = np.array(handler.get_ip_data("147.32.84.165"))
 
 times = [datetime.strptime(infected[i][0], "%Y-%m-%d %H:%M:%S.%f") for i in range(len(infected))]
 protocols = [infected[i][2] for i in range(len(infected))]
-bytes = [infected[i][7] for i in range(len(infected))]
+bytes = [int(infected[i][7]) for i in range(len(infected))]
 
-
-
-print("Show relation between protocol use")
+print("Show usage of protocols")
 plt.plot(times, protocols, color="red")
-plt.plot(times, bytes, color="blue")
 plt.show()
 
 
-print("Show relation between bytes use")
+print("Show the amount of bytes used")
 plt.plot(times, bytes)
 plt.show()
-
-
-
-
