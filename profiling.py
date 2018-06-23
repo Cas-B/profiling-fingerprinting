cb4296aefdc6d053343e38e583d245840d79caf0
Cas-B <s.j.m.buijs@student.tudelft.nl>
2018-06-23 17:33:35 +0200

    Completes profiling mechanics

--- profiling.py
+++ profiling.py
@@ -38,10 +38,52 @@ def createTestData(data, other_hosts, data_handler):
 
 ## Evaluate the trained model using the error based method explained in the paper #######
 def evaluateModel(n_grams_training_data, n_grams_test_data):
-    if len(n_grams_test_data) <= len(n_grams_training_data):
-        return computeError(n_grams_test_data, n_grams_training_data)
+    uniqueSequencesTraining = dict()
+    uniqueSequencesTest = dict()
+
+    #Calculating unique ngrams for training data
+    for ngram in n_grams_training_data:
+        if not ngram in uniqueSequencesTraining.keys():
+            uniqueSequencesTraining[ngram] = 1
+
+    #Calculating unique ngrams for test data
+    for ngram in n_grams_test_data:
+        if not ngram in uniqueSequencesTest.keys():
+            uniqueSequencesTest[ngram] = 1
+
+    if len(uniqueSequencesTraining) <= len(uniqueSequencesTest):
+        return computeErrorNgrams(n_grams_training_data, n_grams_test_data, uniqueSequencesTraining, uniqueSequencesTest)
     else:
-        return computeError(n_grams_training_data, n_grams_test_data)
+        return computeErrorNgrams(n_grams_test_data, n_grams_training_data, uniqueSequencesTest, uniqueSequencesTraining)
+        
+
+    #if len(n_grams_test_data) <= len(uniqueSequencesTest):
+    #    return computeError(n_grams_test_data, n_grams_training_data)
+    #else:
+    #    return computeError(n_grams_training_data, n_grams_test_data)
+
+#### Check if two dictionaries share at least a single key 
+def checkForSharedKey(ngrams_dict_smaller, ngrams_dict_larger):
+    count = 0
+    for ngram in ngrams_dict_smaller.keys():
+        if ngram in ngrams_dict_larger.keys():
+            count += 1
+    
+    if count == 0:
+        return 0
+    else:
+        return 1
+
+#### Compute the error based on what is explained in the paper. #####################
+def computeErrorNgrams(ngrams_dict_count_smaller, ngrams_dict_count_larger, unique_ngrams_smaller, unique_ngrams_larger):
+    if checkForSharedKey(unique_ngrams_smaller, unique_ngrams_larger) == 0:
+        return -1
+
+    error = 0
+    for ngram in unique_ngrams_smaller.keys():
+        if ngram in unique_ngrams_larger.keys():
+            error += (abs(ngrams_dict_count_smaller[ngram] - ngrams_dict_count_larger[ngram]))
+    return error
 
 #### Compute the error based on what is explained in the paper. #####################
 def computeError(ngrams_dict_count_smaller, ngrams_dict_count_larger):
@@ -49,11 +91,12 @@ def computeError(ngrams_dict_count_smaller, ngrams_dict_count_larger):
     for ngram in ngrams_dict_count_smaller:
         if ngram in ngrams_dict_count_larger:
             error += (abs(ngrams_dict_count_smaller[ngram] - ngrams_dict_count_larger[ngram]))
+    return error
 
 
 ############## Read the data ###########################################################
 infected_hosts = ["147.32.84.191", "147.32.84.192", "147.32.84.193", "147.32.84.204", "147.32.84.205", "147.32.84.206", "147.32.84.207", "147.32.84.208", "147.32.84.209"]
-normal_hosts = ["147.32.84.170", "147.32.84.134, 147.32.84.164", "147.32.87.36", "147.32.80.9", "147.32.87.11"]
+normal_hosts = ["147.32.84.170", "147.32.84.134", "147.32.84.164", "147.32.87.36", "147.32.80.9", "147.32.87.11"]
 
 print("Reading in the data")
 data_handler = dataHandler()
@@ -80,7 +123,7 @@ print("Create ngrams (sliding windows) for test data")
 ngrams_test_data = dict()
 
 for host in tqdm(discretised_test_data):
-    ngrams_test_data[host]=createNGrams(discretised_test_data[host], window_size)
+    ngrams_test_data[host] = createNGrams(discretised_test_data[host], window_size)
 
 print("Running trained model on test data")
 
@@ -90,4 +133,18 @@ correctly_identified_normal_hosts = []
 incorrectly_identified_normal_hosts = []
 
 for host in tqdm(ngrams_test_data):
-    evaluateModel(ngrams_training_data, ngrams_test_data[host])
+    error = evaluateModel(ngrams_training_data, ngrams_test_data[host])
+    print("Host: {}, Error: {}".format(host, error))
+    if host in infected_hosts and (error != -1 and error <= 3000):
+        correctly_identified_infected_hosts.append(host)
+    elif host in infected_hosts and (error == -1 or error > 3000):
+        incorrectly_identified_infected_hosts.append(host)
+    elif host in normal_hosts and (error == -1 or error > 3000):
+        correctly_identified_normal_hosts.append(host)
+    elif host in normal_hosts and (error != -1 or error < 3000):
+        incorrectly_identified_normal_hosts.append(host)
+
+print("TP: {}".format(len(correctly_identified_infected_hosts)))
+print("FP: {}".format(len(incorrectly_identified_infected_hosts)))
+print("TN: {}".format(len(correctly_identified_normal_hosts)))
+print("FN: {}".format(len(incorrectly_identified_normal_hosts)))
